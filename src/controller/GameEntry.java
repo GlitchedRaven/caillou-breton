85be27a6acca7cca577b6dd9fd6d101095086673
julien canat <julien.canat@utt.fr>
2016-01-04 20:06:50 +0100

    implemented loading of games, advanced ones not working though.
    GUI not yet implemented

--- src/controller/GameEntry.java
+++ src/controller/GameEntry.java
@@ -1,18 +1,41 @@
 package controller;
 
+import java.io.FileInputStream;
+import java.io.IOException;
+import java.io.ObjectInputStream;
+
+import game.AdvancedGame;
+import game.Game;
+import game.QuickGame;
 //import game.AdvancedGame;
 //import game.QuickGame;
 //import view.Console;
 import view.GameCreatorView;
+import view.GameView;
 
 public class GameEntry {
 	
 	
 
-	public static void main(String[] args) {
-		GameCreatorView test = new GameCreatorView();
+	public static void main(String[] args) throws IOException {
+		//GameCreatorView test = new GameCreatorView();
 		
-		
+		ObjectInputStream ois = null;
+		try {
+			FileInputStream test = new FileInputStream("sauvegarde.txt");
+			ois = new ObjectInputStream(test);
+			Game g = (Game) ois.readObject();		
+			if (g instanceof QuickGame){
+				QuickGameController gc = new QuickGameController((QuickGame) g);
+				GameView gv = gc.getGv();
+			} else {
+				AdvancedGameController gc = new AdvancedGameController((AdvancedGame) g);
+			}
+		} catch (IOException | ClassNotFoundException e) {
+			e.printStackTrace();
+		} finally {
+			ois.close();
+		}
 		
 	
 	}
