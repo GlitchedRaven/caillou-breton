8c1f12409a2c12c5112d50a1adaa5407eccc7346
GlitchedRaven <a0449e79@opayq.com>
2016-01-08 14:51:59 +0100

    Generated Javadoc Skeleton (use JAutodoc)

--- src/card/Ally.java
+++ src/card/Ally.java
@@ -2,30 +2,58 @@ package card;
 
 import java.util.Arrays;
 
+// TODO: Auto-generated Javadoc
+/**
+ * La classe Ally représente une carte allié. Le type précis de cet allié est donné par un attribut.
+ */
 public class Ally extends Card {
-	/**
-	 * 
-	 */
+	
+	
 	private static final long serialVersionUID = 2339940190410564043L;
+	
+	/** The Constant GIANTMOLE. */
 	public final static int GIANTMOLE = 0;
+	
+	/** The Constant WATCHDOG. */
 	public final static int WATCHDOG = 1;
 	
 	
+	/** The Constant TYPES. */
 	public final static int[] TYPES = {GIANTMOLE, WATCHDOG}; 
 	
 
+	/**
+	 * Retourne le type de l'allié
+	 *
+	 * @return the ally type
+	 */
 	public int getAllyType() {
 		return allyType;
 	}
 
+	/** Vecteur représentant la force de l'allié pour chaque saison */
 	private int[] strengthVector;
+	
+	/** Attribut représentant le type de l'allié */
 	private int allyType; 
 	
+	/**
+	 * Retourne le vecteur force.
+	 *
+	 * @return the strength vector
+	 */
 	public int[] getStrengthVector() {
 		return strengthVector;
 	}
 
 
+	/**
+	 * Instantie une nouvelle carte allié
+	 *
+	 * @param name étant le nom de la carte
+	 * @param strengthVector étant le vecteur force
+	 * @param allyType étant le type de l'allié
+	 */
 	public Ally(String name, int[] strengthVector, int allyType) {
 		super(name);
 		this.allyType = allyType;
@@ -33,6 +61,9 @@ public class Ally extends Card {
 		
 	}
 	
+	/* (non-Javadoc)
+	 * @see java.lang.Object#toString()
+	 */
 	@Override
 	public String toString() {
 		return super.getName() + " :  \n\tForce = "  + Arrays.toString(strengthVector) + "\n";
