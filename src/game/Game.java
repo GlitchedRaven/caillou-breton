83978c7ecdf3cd18e1c2cbe2b87671c002b48c3f
julien canat <julien.canat@utt.fr>
2016-01-15 09:45:55 +0100

    started javadoc of the game package

--- src/game/Game.java
+++ src/game/Game.java
@@ -6,35 +6,76 @@ import java.util.ArrayList;
 import java.util.Observable;
 import player.*;
 
-
+/**
+ * Classe représentant la partie, elle possède les méthodes pour changer de saison et de joueurs.
+ */
 public abstract class Game extends Observable implements Serializable{
 	
 	/**
-	 * 
+	 * serialVersionUID nécessaire à la sérialisation
 	 */
 	private static final long serialVersionUID = 5801013053144630175L;
-	
+	/**
+	 * association entre les saisons et des entiers
+	 */
 	public final static int SPRING = 0;
+	/**
+	 * association entre les saisons et des entiers
+	 */
 	public final static int SUMMER = 1;
+	/**
+	 * association entre les saisons et des entiers
+	 */
 	public final static int AUTUMN = 2;
+	/**
+	 * association entre les saisons et des entiers
+	 */
 	public final static int WINTER = 3;
+	/**
+	 * tableau d'association des saisons à leur noms en français
+	 */
 	public final static String[] SEASONS = {"Printemps", "Ete", "Automne", "Hiver"};
-	
+	/**
+	 * entier représentant la saison en cours
+	 */
 	private int season;
+	/**
+	 * booléen déterminant si la partie a été sauvegardée ou non
+	 */
 	private boolean saved;
-	
+	/**
+	 * Arraylist représentant les joueurs de la partie
+	 */
 	protected ArrayList<Player> players;
+	/**
+	 * indice permettant de déterminer qui est le joueur courant
+	 */
 	protected int currentPlayerIndex;
 	
-	
+	/**
+	 * accesseur du booléen saved
+	 * @return boolean
+	 */
 	public boolean isSaved() {
 		return saved;
 	}
-
+	
+	/**
+	 * setter du booléen saved
+	 * @param saved
+	 */
 	public void setSaved(boolean saved) {
 		this.saved = saved;
 	}
 	
+	/**
+	 * Constructeur de la classe Game, il appelle le constructeur des joueurs 
+	 * @param numberAIPlayers : nombre de joueurs IA
+	 * @param aIDifficulties : difficulté des joueurs IA
+	 * @param aINames : nom des joueurs IA
+	 * @param numberHumanPlayers : nombre de joueurs humains
+	 * @param humanNames : nom de joueurs huamins
+	 */
 	public Game(int numberAIPlayers, String[] aIDifficulties, String[] aINames, int numberHumanPlayers, String[] humanNames){
 		//constructor where he doesn't ask stupid questions
 		this.season = SPRING;
@@ -51,35 +92,59 @@ public abstract class Game extends Observable implements Serializable{
 			players.add(new HumanPlayer(humanNames[i], this));
 		}
 	}
-
+	
+	/**
+	 * classe abstraite avec une implémentation différente selon le type de partie
+	 * @return ArrayList<Player>
+	 */
 	public abstract ArrayList<Player> designateWinner(); 
 	
-	
+	/**
+	 * getter de la saison actuelle
+	 * @return int
+	 */
 	public int getSeason() {
 		return season;
 	}
 
 
-
+	/**
+	 * setter de season, notifie les observers en cas de changement
+	 * @param season
+	 */
 	public void setSeason(int season) {
 		this.season = season;
 		this.setChanged();
 		this.notifyObservers();
 	}
-	
+	/**
+	 * getter de l'indice du joueur courant
+	 * @return int
+	 */
 	public int getCurrentPlayerIndex() {
 		return currentPlayerIndex;
 	}
 
-
+	/**
+	 * getter de l'arraylist des joueurs
+	 * @return ArrayList<Player>
+	 */
 	public ArrayList<Player> getPlayers() {
 		return players;
 	}
 	
+	/**
+	 * getter du joueur courant
+	 * @return Player
+	 */
 	public Player getCurrentPlayer() {
 		return players.get(currentPlayerIndex);
 	}
-
+	
+	/**
+	 * setter du joueur courant, notifie les observers en cas de changements
+	 * @param index
+	 */
 	public void setCurrentPlayer(int index) {
 		this.currentPlayerIndex = index;
 		this.setChanged();
